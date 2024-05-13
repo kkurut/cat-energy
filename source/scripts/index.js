64c408f270be8483b7212183212ca724198964db
abdullazim <158089786+kkurut@users.noreply.github.com>
2024-05-13 14:19:25 +0300

    task

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,15 @@
 /* в этот файл добавляет скрипты*/
+const brgBtn = document.querySelector ('.page-header__burger-button');
+const clsBtn = document.querySelector ('.page-header__close-button');
+const navList = document.querySelector ('.page-header__list');
+
+brgBtn.onclick = function () {
+  brgBtn.classList.add ('page-header__burger-button--special');
+  clsBtn.classList.remove ('page-header__close-button--special');
+  navList.classList.remove ('page-header__list--hide');
+};
+clsBtn.onclick = function () {
+  clsBtn.classList.add ('page-header__close-button--special');
+  brgBtn.classList.remove ('page-header__burger-button--special');
+  navList.classList.add ('page-header__list--hide');
+};
