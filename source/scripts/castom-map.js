7a4d00ae145ae277bf9dfdab17153fc484a73831
abdullazim <158089786+kkurut@users.noreply.github.com>
2024-05-20 18:27:37 +0300

    Update castom-map.js

--- source/scripts/castom-map.js
+++ source/scripts/castom-map.js
@@ -1,33 +1,26 @@
 /* eslint-disable */
 ymaps.ready(function () {
-  var myMap = new ymaps.Map(
-      "map",
-      {
+    var myMap = new ymaps.Map("map", {
         center: [59.938631, 30.323037],
         zoom: 16,
         controls: [],
-      },
-      {
-        searchControlProvider: "yandex#search",
-      }
-    ),
-    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-    ),
-    myPlacemark = new ymaps.Placemark(
-      myMap.getCenter(),
-      {
-        hintContent: "Собственный значок метки",
-        balloonContent: "Это красивая метка",
-      },
-      {
-        iconLayout: "default#image",
-        iconImageHref:
-          "../images/map-cat.png",
-        // Размеры метки.
-        iconImageSize: [113, 106],
-        iconImageOffset: [-56.5, -106],
-      }
+        searchControlProvider: "yandex#search"
+    });
+
+    var MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+        '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
     );
-  myMap.geoObjects.add(myPlacemark).add(myPlacemarkWithContent);
+
+    var myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+        hintContent: "Собственный значок метки",
+        balloonContent: "Это красивая метка"
+    }, {
+        iconLayout: "default#image",
+        iconImageHref: "../images/map-cat.png",
+        iconImageSize: [113, 106],
+        iconImageOffset: [-56.5, -106]
+    });
+
+    myMap.geoObjects.add(myPlacemark);
 });
+
