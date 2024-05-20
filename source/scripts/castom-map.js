6d71b5dafa9d06720c267ddb6953d7ca7352d62e
abdullazim <158089786+kkurut@users.noreply.github.com>
2024-05-20 18:41:41 +0300

    Update castom-map.js

--- source/scripts/castom-map.js
+++ source/scripts/castom-map.js
@@ -1,26 +1,22 @@
-/* eslint-disable */
-ymaps.ready(function () {
-    var myMap = new ymaps.Map("map", {
-        center: [59.938631, 30.323037],
-        zoom: 16,
-        controls: [],
-        searchControlProvider: "yandex#search"
-    });
+/* global ymaps */
 
-    var MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-        '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-    );
+ymaps.ready(() => {
+  const myMap = new ymaps.Map('map', {
+    center: [59.938631, 30.323037],
+    zoom: 16,
+    controls: [],
+    searchControlProvider: 'yandex#search'
+  });
 
-    var myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-        hintContent: "Собственный значок метки",
-        balloonContent: "Это красивая метка"
-    }, {
-        iconLayout: "default#image",
-        iconImageHref: "../images/map-cat.png",
-        iconImageSize: [113, 106],
-        iconImageOffset: [-56.5, -106]
-    });
+  const myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+    hintContent: 'Собственный значок метки',
+    balloonContent: 'Это красивая метка'
+  }, {
+    iconLayout: 'default#image',
+    iconImageHref: '../images/map-cat.png',
+    iconImageSize: [113, 106],
+    iconImageOffset: [-56.5, -106]
+  });
 
-    myMap.geoObjects.add(myPlacemark);
+  myMap.geoObjects.add(myPlacemark);
 });
-
